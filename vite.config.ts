58bd883ab5cff9097806dd1cab1c6f13798a8cf2
Nishchal <21051147@kiit.ac.in>
2025-07-24 14:24:39 +0530

    Configured Vite base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -14,6 +39,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/NY-Venture-Website/', // <--- ADDED THIS LINE
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
